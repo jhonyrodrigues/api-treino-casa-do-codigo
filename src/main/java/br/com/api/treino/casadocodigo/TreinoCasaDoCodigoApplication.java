39a0fcf51bc6c2ac1b5c17c03d42dda049fef9c2
Jhony de Oliveira Rodrigues <jhony.o_rodrigues@hotmail.com>
2022-04-21 19:02:30 -0300

    setup inicial

---
+++ src/main/java/br/com/api/treino/casadocodigo/TreinoCasaDoCodigoApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.treino.casadocodigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinoCasaDoCodigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinoCasaDoCodigoApplication.class, args);
+	}
+
+}
